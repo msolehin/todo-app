5ad9c23b71364d29ef57fa21eb2bc6e1ff1dd6de
m.solehin05 <m.solehin05@gmail.com>
2024-06-06 16:21:24 +0800

    Create TodoController with methods for displaying, adding, and resetting to-dos
    - Retrieves the current list of to-dos from the session.
    - Adds the new to-do to the beginning of the list.
    - Saves the updated list back to the session.
    - Clears the list of to-dos from the session.

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,8 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class TodoController extends Controller
 {
-    //
+    public function index(){
+        $todos = Session::get('todos',[]);
+
+        return view('todo',compact('todos'));
+    }
+
+    public function add(Request $request){
+        $request->validate([
+            'todos' => 'required|string|max:255',
+        ]);
+
+        $todos = Session::get('todos',[]);
+        $newTodo = $request->input('todo');
+        array_unshift($todos,$newTodo);
+        Session::put('todos',$todos);
+
+        return redirect('/');
+    }
+
+
+    public function reset(){
+        Session::forget('todos');
+
+        return redirect('/');
+    }
+
 }
